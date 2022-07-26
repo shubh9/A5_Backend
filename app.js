aeedf3032e6b57a048319f9c7696db2ad12756f7
Shubh Mittal <shubhmittal@dhcp-128-189-72-133.ubcsecure.wireless.ubc.ca>
2022-07-26 16:40:23 -0700

    changed cors

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ var receipesRouter = require("./routes/recipes");
 
 var app = express();
 
-app.use(cors());
+// app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
