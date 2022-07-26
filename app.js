8d7c74f67d4183ff9c7a3c9a1515ea898c7d4693
Shubh Mittal <shubhmittal@dhcp-128-189-72-133.ubcsecure.wireless.ubc.ca>
2022-07-26 16:54:48 -0700

    update

--- app.js
+++ app.js
@@ -27,5 +27,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/recipes", receipesRouter);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
 module.exports = app;
