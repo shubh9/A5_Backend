0b83e12bd33e54eb3affe7640be846c8be34a6fd
Shubh Mittal <shubhmittal@dhcp-128-189-72-133.ubcsecure.wireless.ubc.ca>
2022-07-26 17:55:43 -0700

    d

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ var receipesRouter = require("./routes/recipes");
 var app = express();
 
 app.use(cors());
-// const corsOptions ={
-//    origin:'*', 
-//    credentials:true,            //access-control-allow-credentials:true
-//    optionSuccessStatus:200,
-// }
-
-// app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,14 +28,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/recipes", receipesRouter);
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
     
 
 module.exports = app;
