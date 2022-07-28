3a2a73509580277d19af1843fecce8c5cceb7a93
Shubh Mittal <shubhmittal@dhcp-128-189-72-107.ubcsecure.wireless.ubc.ca>
2022-07-28 14:38:11 -0700

    added engines

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ var app = express();
 //hi
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 });
 app.use(logger("dev"));
 app.use(express.json());
